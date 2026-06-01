2434aacb5f2228976ca9cfc50cacb017109c5df6
FantomZ0ng <svaruchandrij@gmail.com>
2026-06-02 00:35:21 +0300

    Landing Screen

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,23 @@
 import { Button } from "@/components/ui/button";
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
+import Header from "./_shared/Header";
+import Hero from "./_shared/Hero";
 
 export default function Home() {
   return (
     <div>
-      <h2> hhhhhhhh</h2>
-      <Button>DDDDD</Button>
-      <UserButton />
+        <Header />
+        <Hero />
+        <div className="absolute -top-40 -left-40 pointer-events-none 
+         h-[500px] w-[500px] bg-purple-400/20 blur-[120px] rounded-full"/>
+
+         <div className="absolute -top-20 right-[-200px] pointer-events-none
+         h-[500px] w-[500px] bg-pink-400/20 blur-[120px] rounded-full"/>
+         <div className="absolute bottom-[-200px] left-1/3 pointer-events-none
+         h-[500px] w-[500px] bg-blue-400/20 blur-[120px] rounded-full"/>
+         <div className="absolute top-[200px] left-1/2 pointer-events-none
+         h-[500px] w-[500px] bg-sky-400/20 blur-[120px] rounded-full"/>
     </div>
   );
 }
